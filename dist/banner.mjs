7bb28eabba5ebfda5a6cd5e8e212c667e3959827
Ajay Singh <ajay@mentormaya.com>
2023-09-26 23:05:03 +0545

    declaration added

--- dist/banner.mjs
+++ dist/banner.mjs
@@ -1,1 +1,1 @@
-import{b as a}from"./chunk-44C2IUCD.mjs";import"./chunk-VZATVPYO.mjs";export{a as default};
+import{b as a}from"./chunk-4TV2ZHG2.mjs";import"./chunk-VZATVPYO.mjs";export{a as default};
